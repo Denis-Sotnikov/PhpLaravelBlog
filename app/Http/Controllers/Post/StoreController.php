1685ec0aaba4a30c86d86e50fd13a91e04542cf3
Denis Sotnikov <sport_piter@mail.ru>
2024-01-11 20:13:47 +0300

    add Service layer

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -2,31 +2,17 @@
 
 namespace App\Http\Controllers\Post;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\StoreRequest;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\PostTag;
-use App\Models\Tag;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 
-class StoreController extends Controller
+
+class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        $tags = $data['tags'];
-        unset($data['tags']);
-        $post = Post::create($data);
-//        foreach ($tags as $tag) {
-//            PostTag::firstOrCreate([
-//                'tag_id' => $tag,
-//                'post_id' => $post->id,
-//            ]);
-//            }0
-        $post->tags()->attach($tags);
+
+        $this->service->store($data);
+
         return redirect()->route('post.index');
     }
 }
